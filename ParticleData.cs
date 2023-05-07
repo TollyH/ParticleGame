5778a8eb1d1158f13c3919622d6b4bba1956d422
TollyH <ptolemyhill@sky.com>
2023-05-07 02:51:13 +0100

    Implement particle data and types

---
+++ ParticleData.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace ParticleGame
+{
+    public class ParticleData
+    {
+        public ParticleTypes.Types ParticleType { get; set; }
+        public Point PreviousPosition { get; set; }
+        public float Age { get; set; }
+
+        public ParticleData(ParticleTypes.Types particleType, Point previousPosition)
+        {
+            ParticleType = particleType;
+            PreviousPosition = previousPosition;
+            Age = 0;
+        }
+    }
+}
---
+++ ParticleTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace ParticleGame
+{
+    public static class ParticleTypes
+    {
+        public enum Types
+        {
+            Air,
+            Block,
+            Water,
+            Sand,
+            RedSand,
+            Lava,
+            Steam,
+            Magma
+        }
+
+        public static readonly Types[] ParticleTypeArray = new Types[]
+        {
+            Types.Air, Types.Block, Types.Water,
+            Types.Sand, Types.RedSand, Types.Lava,
+            Types.Steam, Types.Magma
+        };
+    }
+}
